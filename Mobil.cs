27b5e6e17141832d99eecb96e6acedf5883ba466
linggarelang <linggar.elangp@students.amikom.ac.id>
2023-03-31 23:19:24 +0700

    modified class mobil.cs

--- Mobil.cs
+++ Mobil.cs
@@ -7,12 +7,88 @@ namespace myApps
 {
     public class Mobil
     {
-        public string Warna { get; set; }
-        public string jumlahPintu { get; set; }
-        public string Merk { get; set; }
-        public string Model { get; set; }
-        public string tahunKeluar { get; set; }
-        public double Kecepatan { get; set; }
+
+        // Field
+        private string _Warna;
+        private string _jumlahPintu;
+        private string _Merk;
+        private string _Model;
+        private string _tahunKeluar;
+        private double _Kecepatan;
+
+        // Property
+        public string Warna
+        {
+            get
+            {
+                return _Warna;
+            }
+
+            set
+            {
+                _Warna = value;
+            }
+        }
+        public string jumlahPintu
+        {
+            get
+            {
+                return _jumlahPintu;
+            }
+
+            set
+            {
+                _jumlahPintu = value;
+            }
+        }
+        public string Merk
+        {
+            get
+            {
+                return _Merk;
+            }
+
+            set
+            {
+                _Merk = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return _Model;
+            }
+
+            set
+            {
+                _Model = value;
+            }
+        }
+        public string tahunKeluar
+        {
+            get
+            {
+                return _tahunKeluar;
+            }
+
+            set
+            {
+                _tahunKeluar = value;
+            }
+        }
+        public double Kecepatan
+        {
+            get
+            {
+                return _Kecepatan;
+            }
+
+            set
+            {
+                _Kecepatan = value;
+            }
+        }
 
         // My Method
         public void kecepatanMobil()
@@ -30,12 +106,5 @@ namespace myApps
 
             Console.WriteLine($"Mobil saya berwarna {Warna}, Merek {Merk}, Model {Model}, Keluaran Tahu {tahunKeluar}, dengan jumlah pintu sebanyak {jumlahPintu} Pintu");
         }
-
-
-
-    }
-
-    public class Kecepatan
-    {
     }
 }
